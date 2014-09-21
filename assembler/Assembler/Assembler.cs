e2ec2055c2e58ae00147fcac96e2c856e5d35b8a
pavlosmcg <pavlosmcg@users.noreply.github.com>
2014-09-21 22:50:25 +0100

    Add empty c# assembler solution

---
+++ assembler/Assembler/Assembler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assembler
+{
+    public class Assembler
+    {
+        public string[] Assemble(string[] lines)
+        {
+            var assembled = lines.Select().Where(l => !string.IsNullOrEmpty(l)).ToArray();
+
+            return assembled;
+        }
+    }
+}
---
+++ assembler/Assembler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Please specifiy a file path");
+                return;
+            }
+
+            var assember = new Assembler();
+
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            string[] assembledLines = assember.Assemble(lines);
+
+            foreach (var line in assembledLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
