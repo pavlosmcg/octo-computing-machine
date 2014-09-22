3324106f90ebdf8f0004be7b1651528dc94d0caf
pavlosmcg <pavlosmcg@users.noreply.github.com>
2014-09-22 21:33:00 +0100

    Test that assembler removes empty lines

---
+++ assembler/Assembler.Tests/AssemblerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Assembler.Tests
+{
+    [TestFixture]
+    public class AssemblerTests
+    {
+        [Test]
+        public void Assembler_Removes_Empty_Lines()
+        {
+            // arrange
+            string[] inputLines = {"blorg", "fester", "", "framistan"};
+            var assember = new Assembler();
+
+            // act
+            string[] assembledLines = assember.Assemble(inputLines);
+
+            // assert
+            Assert.AreEqual(3, assembledLines.Length);
+        }
+
+    }
+}
